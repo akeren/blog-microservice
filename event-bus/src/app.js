fe2ade0b44d0a693014914d9ada2455f0a924b8c
Kater Akeren <akerenkater@gmail.com>
2021-12-24 00:55:23 +0100

    Refactor

--- event-bus/src/app.js
+++ event-bus/src/app.js
@@ -9,9 +9,9 @@ app.post('/api/v1/events', (req, res) => {
 	const event = req.body;
 
 	axios.post('http://posts-clusterip-service:4000/api/v1/events', event);
-	axios.post('http://localhost:4001/api/v1/events', event);
-	axios.post('http://localhost:4002/api/v1/events', event);
-	axios.post('http://localhost:4003/api/v1/events', event);
+	axios.post('http://comments-service:4001/api/v1/events', event);
+	axios.post('http://query-service:4002/api/v1/events', event);
+	axios.post('http://moderation-service:4003/api/v1/events', event);
 
 	res.status(200).json({
 		status: true,
